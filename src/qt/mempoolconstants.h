ea825cc9248794e977e4bd94c3142753dbc057ca
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-11-21 05:15:40 -0500

    graph only - for layout design quiet

--- src/qt/mempoolconstants.h
+++ src/qt/mempoolconstants.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <qt/mempoolstats.h>
 
 static const char *LABEL_FONT = "Roboto Mono";
